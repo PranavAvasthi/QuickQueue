500b3ce3e50ba18f4b375d78900efe42f102cf7f
PranavAvasthi <pranavavasthi44@gmail.com>
2025-07-24 14:15:12 +0530

    nativewind setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,24 +5,15 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { Text, View } from 'react-native';
+import '../global.css';
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-green-500">
+        Nativewind Setup
+      </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
-export default App;
