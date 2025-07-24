f6bfe9c99ae056fe4508dcfd7d203d78dce4b730
PranavAvasthi <pranavavasthi44@gmail.com>
2025-07-24 14:39:17 +0530

    server creation

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,16 @@
-console.log("Hello World");
+import express from "express";
+import http from "http";
+import cors from "cors";
+import { PORT } from "./config/env";
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(cors());
+app.get("/", (_req, res) => {
+  res.send("Server Health Check");
+});
+
+server.listen(PORT, () => {
+  console.log(`✅ Server listening on port ${PORT}`);
+});
